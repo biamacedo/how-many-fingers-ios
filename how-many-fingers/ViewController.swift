b30cac660b89be02ab978b78ccf7a106b6a6620b
bianet13 <bia.macedo13@gmail.com>
2015-09-27 13:44:30 -0300

    init commit

--- how-many-fingers/ViewController.swift
+++ how-many-fingers/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userGuessTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func guess() {
+        
+        let diceRoll = Int(arc4random_uniform(5) + 1)
+        if userGuessTextField.text! != "" && diceRoll == Int(userGuessTextField.text!) {
+            resultLabel.text = "You're Right!"
+        } else {
+            resultLabel.text = "Wrong! It was a \(diceRoll)"
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
